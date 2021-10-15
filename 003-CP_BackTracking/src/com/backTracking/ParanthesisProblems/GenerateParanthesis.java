fe9d779ec48f31d35447c4a97d0601d2e8d98ad5
Biswaranjan Rout <biswaranjan.brout@gmail.com>
2021-10-15 20:21:51 +0530

    Removed Sysout from GenerateParanthesis.java

--- 003-CP_BackTracking/src/com/backTracking/ParanthesisProblems/GenerateParanthesis.java
+++ 003-CP_BackTracking/src/com/backTracking/ParanthesisProblems/GenerateParanthesis.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class GenerateParanthesis {
 	public static void main(String[] args) {
-		
+
 		List<String> brackets = new ArrayList<>();
 		int n = 3;
 
@@ -15,33 +15,29 @@ public class GenerateParanthesis {
 	}
 
 	private static void backTracking(List<String> brackets, StringBuilder cur, int open, int close, int n) {
-		System.out.println("open: "+open+", close: "+close+": "+cur);
 		// When cur.length == 2 * n, then add in the list and return
 		if (cur.length() == 2 * n) {
 			brackets.add(cur.toString());
-			System.out.println("List: "+brackets);
 		}
-		
+
 		// If open count < n then, add more open bracket
 		if (open < n) {
 			cur.append("(");
 			// append till we reach length == n. Ex: (((
 			backTracking(brackets, cur, open + 1, close, n);
 			cur.deleteCharAt(cur.length() - 1);
-			
+
 		}
-		System.out.println("******************************** ");
 		/*
 		 * If close count < open count, that means we can add more close bracket. This
 		 * condition will only run when we have open bracket
 		 */
-		System.out.println("close: "+close+": "+cur+", open: "+open );
 		if (close < open) {
 			cur.append(")");
 			// closing bracket length should be equal to open length, not more.
 			backTracking(brackets, cur, open, close + 1, n);
 			cur.deleteCharAt(cur.length() - 1);
 		}
-		
+
 	}
 }
